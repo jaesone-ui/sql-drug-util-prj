df0f8173459e0f521e7c3d0bfa468c2ae025cb15
Jason <jaesone@vt.edu>
2026-03-01 01:25:52 -0500

    upper() to group same drug entries

--- 02_data_cleaning.sql
+++ 02_data_cleaning.sql
@@ -1,11 +1,12 @@
+
 CREATE TABLE drug_util_2025_clean AS
     SELECT 
         us_state,
-        product_name,
+        UPPER(TRIM(product_name)) AS product_name, -- ensures similar entries are correctly grouped
         units_reimbursed,
         prescription_count,
         total_reimbursed,
         medicaid_reimbursed,
         non_medicaid_reimbursed
     FROM drug_util_2025
-    WHERE suppress_used='FALSE'; -- drug details are null when suppressed
+    WHERE suppress_used='FALSE'; -- drug metrics are null when suppressed

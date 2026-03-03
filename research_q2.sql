7b57935b3c0515cc2c64ec94ccdd964fa3f7734b
Jason <jaesone@vt.edu>
2026-03-02 23:47:27 -0500

    fixed drug repeats on q2

--- research_q2.sql
+++ research_q2.sql
@@ -1,12 +1,21 @@
 -- Q2: What are the top 5 most prescribed drugs per state?
 
-WITH top_per_state AS (
+
+WITH same_drug AS (
+    SELECT
+        us_state,
+        product_name,
+        SUM(prescription_count) AS distinct_prescriptions
+        FROM drug_util_2025_clean
+        GROUP BY us_state, product_name
+),
+top_per_state AS (
     SELECT 
         us_state,
         product_name AS drug_name,
-        prescription_count,
-        RANK() OVER(PARTITION BY us_state ORDER BY prescription_count DESC) AS ps_rank
-    FROM drug_util_2025_clean
+        distinct_prescriptions AS prescription_count,
+        RANK() OVER(PARTITION BY us_state ORDER BY distinct_prescriptions DESC) AS ps_rank
+    FROM same_drug
 )
 
 SELECT *
